1997a9f8d0e133b36e764db5f4e4794494770c1e
William <william.teletabs@hotmail.com>
2022-06-13 16:45:48 -0300

    Deixando o ítem de index 0 sempre visível com a classe 'ativo'

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
+tabContent[0].classList.add('ativo');
+
 
 function activeTab(index){
   tabContent.forEach((section) =>{
